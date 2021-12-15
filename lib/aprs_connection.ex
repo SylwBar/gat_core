0f872bc99acb45ce659d71c0eab964f013d96d2a
Sylwester Barański <sylw.bar@gmail.com>
2021-12-15 13:31:00 +0100

    APRSClient comments

--- lib/aprs_connection.ex
+++ lib/aprs_connection.ex
@@ -6,6 +6,8 @@ defmodule APRSConnection do
   require Logger
 
   @impl true
+  # ----- APRS Connection process init. function -----
+
   def init([aprs_config, core_server_name]) do
     server_addr = Map.get(aprs_config, "server_addr")
     server_port = Map.get(aprs_config, "server_port")
@@ -17,8 +19,8 @@ defmodule APRSConnection do
       client_id: client_id,
       core_server_name: core_server_name,
       socket: nil,
-      pkt_fragment: <<>>,
-      ka_timer: nil
+      ka_timer: nil,
+      pkt_fragment: <<>>
     }
 
     send(self(), :connect)
@@ -26,9 +28,14 @@ defmodule APRSConnection do
   end
 
   @impl true
+  # ----- APRS connection attempt -----
   def handle_info(:connect, state) do
-    Logger.info("APRSConnection: connection attempt")
     {:ok, server_ip4} = state.server_addr |> String.to_charlist() |> :inet.getaddr(:inet)
+    {ip1, ip2, ip3, ip4} = server_ip4
+
+    Logger.info(
+      "APRSConnection: connection attempt to #{state.server_addr}, IP: #{ip1}.#{ip2}.#{ip3}.#{ip4}"
+    )
 
     {socket, ka_timer} =
       case :gen_tcp.connect(server_ip4, state.server_port, [:binary, active: true]) do
@@ -52,11 +59,7 @@ defmodule APRSConnection do
     {:noreply, %{state | socket: socket, pkt_fragment: <<>>, ka_timer: ka_timer}}
   end
 
-  def handle_info(:send_keep_alive, state) do
-    :ok = :gen_tcp.send(state.socket, "#KA\r\n")
-    {:noreply, state}
-  end
-
+  # ----- TCP data received -----
   def handle_info({:tcp, _socket, packet}, state) do
     packet_list =
       <<state.pkt_fragment::binary, packet::binary>>
@@ -69,16 +72,27 @@ defmodule APRSConnection do
     {:noreply, %{state | pkt_fragment: pkt_fragment}}
   end
 
+  # ----- Keep alive messages -----
+  def handle_info(:send_keep_alive, state) do
+    :ok = :gen_tcp.send(state.socket, "#KA\r\n")
+    {:noreply, state}
+  end
+
+  # ----- TCP connection down -----
   def handle_info({:tcp_closed, _socket}, state) do
     Logger.warning("APRSConnection: connection closed")
-    if (state.ka_timer != nil) do
+
+    if state.ka_timer != nil do
       :timer.cancel(state.ka_timer)
     end
+
     :timer.send_after(@reconnect_timer_sec * 1000, :connect)
     {:noreply, %{state | socket: nil, ka_timer: nil}}
   end
 
-  defp handle_packet(<<"#", comment::binary>>), do: Logger.debug("APRSConnection: ##{comment}")
+  # ----- APRC Client private functions -----
+  defp handle_packet(<<"# logresp", _::bytes>> = pkt), do: Logger.info("APRSConnection: #{pkt}")
+  defp handle_packet(<<"#", _::bytes>> = pkt), do: Logger.debug("APRSConnection: #{pkt}")
   defp handle_packet(_), do: :ok
 
   # ----- APRSConnection API -----
