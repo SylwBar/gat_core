33ae45093b5eb4415738b204f6c5b47ace0eb417
Sylwester Barański <sylw.bar@gmail.com>
2022-01-08 22:54:42 +0100

    Test added

--- test/ogn_core_test.exs
+++ test/ogn_core_test.exs
@@ -2,9 +2,16 @@ defmodule OGNCoreTest do
   use ExUnit.Case
   doctest OGNCore
 
-  test "APRS check" do
+  test "APRS check #1" do
     assert OGNCore.APRS.get_source_id(
              "TEST>OGNSDR,TCPIP*,qAC,GLIDERN1:/161412h1020.30NI01234.56E&/A=000321"
-           ) == {:station, "TEST"}
+           ) == {:ogn_station, "TEST"}
   end
+
+  test "APRS check #2" do
+    assert OGNCore.APRS.get_source_id(
+             "FLRAABBCC>APRS,qAS,TEST:/123456h1234.56S/12345.67W'123/321/A=123456 !W06! id06AABBCC -355fpm -3.3rot 2.8dB 5e +1.0kHz gps3x3"
+           ) == {:ogn_object, {2, "\xAA\xBB\xCC"}, :flarm}
+  end
+
 end
